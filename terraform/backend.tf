e7efb98e824ea4e777956274a7f74d85ea74f71d
Robert A. Hill <robert@decentlabs.com>
2022-01-19 20:34:52 -0800

    move state

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "rhill-terraform"
-    key    = "personal/terraform.tfstate"
+    key    = "infrastructure/terraform.tfstate"
     region = "us-west-2"
   }
   required_providers {
@@ -15,35 +15,4 @@ terraform {
 
 provider "aws" {
   region = "us-west-2"
-}
-
-resource "aws_s3_bucket" "terraform" {
-  bucket        = "rhill-terraform"
-  acl           = "private"
-  force_destroy = false
-
-  versioning {
-    enabled = true
-  }
-
-  logging {
-    target_bucket = data.aws_s3_bucket.terraform.id
-    target_prefix = "logs/"
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.terraform.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
 }
